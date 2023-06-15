002f4abab5a5e45b517f2ed568450d57f38e4926
jmlago <josemlagoalonso@gmail.com>
2023-06-15 13:42:39 +0000

    wokring on replit

--- .replit_config/main.py
+++ .replit_config/main.py
@@ -1,1 +1,11 @@
 import subprocess
+
+subprocess.Popen(["python3", "genworlds/sockets/world_socket_server.py"],
+                 stdout=subprocess.DEVNULL,
+                 stderr=subprocess.STDOUT)
+subprocess.Popen([
+    "python3", "use_cases/roundtable/world_setup_n_launch_tree_of_thought.py"
+],
+                 stdout=subprocess.DEVNULL,
+                 stderr=subprocess.STDOUT)
+subprocess.run(["python3", "genworlds/interfaces/cli/run.py"])
