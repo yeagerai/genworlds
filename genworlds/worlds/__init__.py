6798bd3eb91e48e0382371b957ede8453bdc6cfb
jmlago <josemlagoalonso@gmail.com>
2023-09-14 21:14:49 +0100

    blacked

--- genworlds/worlds/__init__.py
+++ genworlds/worlds/__init__.py
@@ -1,2 +1,4 @@
 from genworlds.worlds.location_world.location_world import LocationWorld
-from genworlds.worlds.chat_interface_world.chat_interface_world import ChatInterfaceWorld
+from genworlds.worlds.chat_interface_world.chat_interface_world import (
+    ChatInterfaceWorld,
+)
